ab2c192c10bedde2a8c4cdf0cefb5f96a0c10955
TrostAft <abhijit9331@gmail.com>
2016-03-03 12:18:36 -0500

    got url param working

--- teamdata.py
+++ teamdata.py
@@ -2,17 +2,15 @@ import urllib.request
 import json
 import pprint
 
+URL = 'https://www.thebluealliance.com/api/v2/'
+HEADER_KEY = '?X-TBA-App-Id='
+HEADER_VAL = 'frc2849:scouting-machine:v01'
 
-URL = 'http://www.thebluealliance.com/api/v2/'
-HEADER_KEY = 'X-TBA-App-Id'
-HEADER_VAL = 'frc2849:scouting-machine:1'
-
-
-# returns a list of the teams (team key) participating in an event
+# returns a list of the teams (FRCXXXX) participating in an event
 def get_event_teams(event_key):
-	request = urllib.request.Request(URL + 'event/' + event_key + '/teams')
-	request.add_header(HEADER_KEY, HEADER_VAL)
-	response = urllib.request.urlopen(request)
+	user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+	#request = urllib.request.Request(URL + 'event/' + event_key + '/teams', HEADER_KEY, HEADER_VAL)
+	response =
 	jsonified = json.loads(response.read().decode("utf-8"))
 	teams = []
 
@@ -21,5 +19,6 @@ def get_event_teams(event_key):
 
 	return teams
 
-def main():
-    print get_event_teams('2010sc')
+
+
+print (get_event_teams('2010sc'))
